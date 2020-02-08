6ee931fed38008123289725128027ebd8b6cc885
Chris-Hughes-Echo <chris.hughes.echo@gmail.com>
2020-02-08 16:03:14 -0600

    add chris-prototype scene

---
+++ Ultimate Robot Human/Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D colObj)
+    {
+        if (colObj.gameObject.tag.Equals("Lethal"))
+        {
+            Debug.Log("Death");
+        }
+        if (colObj.gameObject.tag.Equals("Finish"))
+        {
+            Debug.Log("Success");
+        }
+    }
+}

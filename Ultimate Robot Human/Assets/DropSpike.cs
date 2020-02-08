9950335f7733100e5ab96031543cc33e8bb1be7e
Linda Carey <lhcarey09@gmail.com>
2020-02-08 13:04:29 -0800

    menu

---
+++ Ultimate Robot Human/Assets/DropSpike.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropSpike : MonoBehaviour
+{
+    Rigidbody2D rigidbody; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.GetComponent<Rigidbody2D> ();
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals ("AI"))
+        rigidbody.isKinematic = false;
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.name.Equals ("AI"))
+            Debug.Log("Spike Hits!");
+    }
+}
---
+++ Ultimate Robot Human/Assets/MoveChar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChar : MonoBehaviour
+{
+    float dirX;
+    [SerializeField]
+    float moveSpeed = 5f;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.position = new Vector2 (transform.position.x + dirX, transform.position.y);
+    }
+}

4a8aabcb0fbd758683f3ed654c79962e43517602
Kendrick Chu <kendrickqchu@gmail.com>
2020-02-09 15:32:57 -0800

    Prototype training area and reorganization.

---
+++ Ultimate Robot Human/Assets/Scripts/SpawnFinish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFinish : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] GameObject finishPrefab;
+    int randomSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+        Instantiate(finishPrefab, spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

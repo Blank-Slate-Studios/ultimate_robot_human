e1a2c13d7b03d5a85cf22cbb9d227e7cfc9f2108
Chris-Hughes-Echo <chris.hughes.echo@gmail.com>
2020-02-15 14:33:42 -0600

    added new restart script that uses active scene instead of hardcoded name. Added toolbar empty object, buttons, and script to place block in scene at runtime.

---
+++ Ultimate Robot Human/Assets/Scripts/BlockButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockButtonScript : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void spawnBlock()
+    {
+        GameObject block = Instantiate(blockPrefab) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

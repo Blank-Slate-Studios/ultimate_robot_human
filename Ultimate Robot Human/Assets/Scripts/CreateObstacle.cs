71c8c4829d3f13411189ef63b5c1912ae853f0b4
Kendrick Chu <kendrickqchu@gmail.com>
2020-02-22 13:04:35 -0800

    Created obstacle creation button and drag and drop script.

---
+++ Ultimate Robot Human/Assets/Scripts/CreateObstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CreateObstacle : MonoBehaviour, IPointerDownHandler
+{
+    //Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] Transform obstaclePosition;
+    GameObject newObstacle;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //Vector3 worldPos;
+        //Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit, 1000f))
+        //{
+        //    worldPos = hit.point;
+        //}
+        //else
+        //{
+        //    worldPos = Camera.main.ScreenToWorldPoint(mousePosition);
+        //}
+        newObstacle = Instantiate(obstaclePrefab, obstaclePosition.position, Quaternion.identity);
+    }
+}

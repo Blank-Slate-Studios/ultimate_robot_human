e1d651c87ea0202eed5c4694eab80cdae1ad98d0
Chris-Hughes-Echo <chris.hughes.echo@gmail.com>
2020-03-01 20:43:55 -0600

    further modified PlayerAgent.cs, added TFModels and 1st run training model

--- Ultimate Robot Human/Assets/Scripts/PlayerAgent.cs
+++ Ultimate Robot Human/Assets/Scripts/PlayerAgent.cs
@@ -5,6 +5,14 @@ using MLAgents;
 
 public class PlayerAgent : Agent
 {
+    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
+
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+
     public bool lethalCol = false;
     public bool finishCol = false;
 
@@ -19,8 +27,21 @@ public class PlayerAgent : Agent
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-        Debug.Log(rigid.velocity);
-        Debug.Log(rigid.velocity.x);        
+    }
+
+    private bool isGrounded()
+    {
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void OnCollisionEnter2D(Collision2D colObj)
@@ -28,8 +49,6 @@ public class PlayerAgent : Agent
         if (colObj.gameObject.tag.Equals("Lethal"))
         {
             //Debug.Log("Death");
-            //gameOverText.SetActive(true);
-            //restartButton.SetActive(true);
             //gameStop = true;
             //rigid.velocity = Vector2.zero;
             lethalCol = true;
@@ -37,9 +56,6 @@ public class PlayerAgent : Agent
         if (colObj.gameObject.tag.Equals("Finish"))
         {
             //Debug.Log("Success");
-            //winText.SetActive(true);
-            //restartButton.SetActive(true);
-            //nextLevelButton.SetActive(true);
             //gameStop = true;
             //rigid.velocity = Vector2.zero;
             finishCol = true;
@@ -63,7 +79,7 @@ public class PlayerAgent : Agent
 
         }
 
-        // Address finish line collision later
+        // Address finish line collision
         if (finishCol)
         {
             rigid.velocity = Vector2.zero;
@@ -87,14 +103,30 @@ public class PlayerAgent : Agent
         AddVectorObs(rigid.velocity.y);
     }
 
-    public float speed = 10f;
+    public float speed = 40f;
+    public float jumpForce = 700f;
+    private int directionX = 0;
+    private int directionY = 0;
     public override void AgentAction(float[] vectorAction)
     {
+        int movement = Mathf.FloorToInt(vectorAction[0]);
+        int jump = Mathf.FloorToInt(vectorAction[1]);
+
+        if (movement == 1) { directionX = -1; }
+        if (movement == 2) { directionX = 1; }
+        // Look up index in jump action list
+        if (jump == 1 && isGrounded()) { directionY = 1; }
+
+        //rigid.AddForce(new Vector2(directionX * speed, directionY * jumpForce));
+        rigid.AddForce(new Vector2(directionX * speed, 0));
+        directionY = 0;
+        directionX = 0;
+
         // Actions size = 1 or 2?
-        Vector2 controlSignal = Vector2.zero;
-        controlSignal.x = vectorAction[0];
-        controlSignal.y = vectorAction[1];
-        rigid.AddForce(controlSignal * speed);
+        //Vector2 controlSignal = Vector2.zero;
+        //controlSignal.x = vectorAction[0];
+        //controlSignal.y = vectorAction[1];
+        //rigid.AddForce(controlSignal * speed);
 
         // Rewards
         if (finishCol)
@@ -117,6 +149,7 @@ public class PlayerAgent : Agent
         var action = new float[2];
         action[0] = Input.GetAxis("Horizontal");
         action[1] = Input.GetAxis("Vertical");
+        //action[1] = Mathf.FloorToInt(Input.GetButtonDown("Jump"));
         return action;
     }
 }

8f14aa8081a2e1b6cd22d540b87b423299a75707
Linda Carey <lhcarey09@gmail.com>
2020-03-12 20:49:58 -0700

    level2

---
+++ Ultimate Robot Human/Assets/Scripts/FrogMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogMove : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    public float wallLeft = 0.0f;
+    public float wallRight = 5.0f;      
+    float walkingDirection = 1.0f;
+    Vector2 walkAmount;
+
+
+    void Start()
+    {
+        wallLeft = transform.position.x - 2.5f;
+        wallRight = transform.position.x + 2.5f;
+    }
+
+    void Update()
+    {
+        walkAmount.x = walkingDirection * moveSpeed * Time.deltaTime;
+        if (walkingDirection > 0.0f && transform.position.x >= wallRight)
+        {
+            walkingDirection = -1.0f;
+        }
+        else if (walkingDirection < 0.0f && transform.position.x <= wallLeft)
+        {
+            walkingDirection = 1.0f;
+        }
+        transform.Translate(walkAmount);
+    }
+}
